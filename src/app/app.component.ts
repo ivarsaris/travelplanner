d6e3ee29724ffcc9c514c76f889de3fa62921d81
Ivar <ivar.saris@blueelevation.nl>
2025-03-26 14:26:47 +0100

    set up angular 17 project, install tailwind

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppComponent {
+  
+}
