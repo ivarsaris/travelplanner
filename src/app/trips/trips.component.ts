e85934daf09e4790702084ef06e90f528d9c88f8
Ivar <ivar.saris@blueelevation.nl>
2025-03-27 16:41:13 +0100

    add routerlink to trip detail page

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -3,11 +3,12 @@ import { Subscription } from 'rxjs';
 import { Trip } from './trip.model';
 import { TripsService } from './trips.service';
 import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-trips',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, RouterLink],
   templateUrl: './trips.component.html',
   styleUrl: './trips.component.scss'
 })
