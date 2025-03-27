8e80431bca2ef194dd894e1d6488fe9fea508d00
Ivar <ivar.saris@blueelevation.nl>
2025-03-27 14:07:13 +0100

    implement routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { DisplayRouteMapComponent } from './display-route-map/display-route-map.component';
 import { PlacesSearchComponent } from './places-search/places-search.component';
+import { TripsComponent } from './trips/trips.component';
+import { TripDetailComponent } from './trips/trip-detail/trip-detail.component';
+import { RouterLink, RouterLinkActive, RouterOutlet, Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DisplayRouteMapComponent, PlacesSearchComponent],
+  imports: [DisplayRouteMapComponent, PlacesSearchComponent, RouterOutlet, RouterLink, RouterLinkActive, TripsComponent, TripDetailComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  
+
+  constructor(private router: Router) { }
+
+  getCurrentRoute() {
+    switch (this.router.url) {
+      case '/trips':
+        return 'Trips';
+        break;
+      case '/trip-details':
+        return 'Trip Details';
+        break;
+      default:
+        return '';
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TripsComponent } from './trips/trips.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'trips',
+        component: TripsComponent,
+    }
+];
