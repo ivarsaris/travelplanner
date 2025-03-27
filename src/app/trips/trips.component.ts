19a01050968b40d04b89a7ee2c38009e78281905
Ivar <ivar.saris@blueelevation.nl>
2025-03-27 14:06:55 +0100

    create trips component, display overview of trips imported from trips service

---
+++ src/app/trips/trips.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Trip } from './trip.model';
+import { TripsService } from './trips.service';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-trips',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './trips.component.html',
+  styleUrl: './trips.component.scss'
+})
+export class TripsComponent {
+  private tripsService = inject(TripsService);
+  private tripsListSubscription!: Subscription;
+  tripsList: Trip[] = [];
+
+  ngOnInit() {
+    this.tripsListSubscription = this.tripsService.tripsList$.subscribe(tripsList => {
+      this.tripsList = tripsList;
+    });
+  }
+}
