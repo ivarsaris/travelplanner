693c15c2ed1c5648112dfa02c584150b81e3694b
Ivar <ivar.saris@blueelevation.nl>
2025-04-21 14:23:04 +0200

    create first http call, trips service http call WIP

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Trip } from './trip.model';
 import { TripsService } from './trips.service';
 import { NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-trips',
@@ -12,14 +13,24 @@ import { RouterLink } from '@angular/router';
   templateUrl: './trips.component.html',
   styleUrl: './trips.component.scss'
 })
-export class TripsComponent {
+export class TripsComponent implements OnInit {
   private tripsService = inject(TripsService);
   private tripsListSubscription!: Subscription;
   tripsList: Trip[] = [];
+  private httpClient = inject(HttpClient);
 
   ngOnInit() {
     this.tripsListSubscription = this.tripsService.tripsList$.subscribe(tripsList => {
       this.tripsList = tripsList;
+    });
+
+    this.httpClient.get<Trip[]>('http://localhost:3000/trips-list').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error('Error fetching trips:', error);
+      }
     });
   }
 
--- src/app/trips/trips.service.ts
+++ src/app/trips/trips.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from "@angular/core";
+import { Injectable, OnInit, inject } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { Trip } from "./trip.model";
 import { tripsList } from "./trips.list";
 import { TripStop } from "./trip-stop.model";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({ providedIn: 'root' })
 
-export class TripsService {
+export class TripsService implements OnInit {
     private tripsList = new BehaviorSubject<Trip[]>(tripsList);
     tripsList$ = this.tripsList.asObservable();
+
+    private httpClient = inject(HttpClient);
+
+    ngOnInit() {
+        this.httpClient.get<Trip[]>('http://localhost:3000/trips-list').subscribe({
+            next: (response) => {
+                console.log(response);
+            }
+        });
+    }
 
     getTripById(id: string) {
         return this.tripsList.value.find(trip => trip.id === id);
