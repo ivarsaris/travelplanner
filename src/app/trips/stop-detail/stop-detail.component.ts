5e0bf8d63b8aad6198e2017f177e45bf3609f675
Ivar <ivar.saris@blueelevation.nl>
2025-05-30 15:18:41 +0200

    retrieve list of hotels near trip stop and display markers on map

--- src/app/trips/stop-detail/stop-detail.component.ts
+++ src/app/trips/stop-detail/stop-detail.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { NgModule } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { GoogleMapsModule, MapMarker } from '@angular/google-maps';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-stop-detail',
   standalone: true,
-  imports: [GoogleMapsModule],
+  imports: [GoogleMapsModule, MapMarker],
   templateUrl: './stop-detail.component.html',
   styleUrl: './stop-detail.component.scss'
 })
 export class StopDetailComponent implements AfterViewInit {
+  place: any;
   center: google.maps.LatLngLiteral | undefined = undefined;
-  zoom = 8;
-  markers = [
-    { lat: 40.73061, lng: -73.935242 },
-    { lat: 40.74988, lng: -73.968285 }
-  ];
+  zoom = 12;
+  // markerPositions: { lat: number, lng: number }[] = [];
+  markerPositions: google.maps.LatLngLiteral[] = [];
 
   googlePlaceID!: string | null;
   googlePlacesService?: google.maps.places.PlacesService;
@@ -48,6 +47,7 @@ export class StopDetailComponent implements AfterViewInit {
 
           if (place?.geometry?.location) {
 
+            this.place = place;
             this.center = { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() };
           }
         });
@@ -55,5 +55,35 @@ export class StopDetailComponent implements AfterViewInit {
         console.error('error fetching place: ' + status);
       }
     });
+  }
+
+  fetchHotelsNearStop() {
+    const request = {
+      // required parameters
+      location: this.center,
+      fields: ['displayName', 'location', 'businessStatus'],
+      locationRestriction: {
+        center: this.center,
+      },
+      radius: 2000,
+      includedPrimaryTypes: ['lodging'],
+      maxResultCount: 5,
+    };
+
+    this.googlePlacesService?.nearbySearch(request, (places, status) => {
+      if (status === 'OK' && places && places.length > 1) {
+        this.ngZone.run(() => {
+          console.log(places);
+
+          places.forEach((place) => {
+            if (place.business_status === 'OPERATIONAL') {
+              this.markerPositions.push(place.geometry!.location!.toJSON());
+            }
+          });
+        })
+      } else {
+        console.error('error fetching hotels: ' + status);
+      }
+    })
   }
 }
