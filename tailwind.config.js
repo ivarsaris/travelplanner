2961f6dd7956103e5ae3acfddd8d621a32b4ef7f
Ivar <ivar.saris@blueelevation.nl>
2025-06-05 11:23:29 +0200

    add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
- content: ["./src/**/*.{html,js}"],
- theme: {
-   extend: {
-     colors: {
-       red: '#dc2626',
-       red_light: '#e35151',
-       red_dark: '#b01e1e',
-       blue: '#2626dc',
-       blue_light: '#6767e6',
-       blue_dark: '#1a1a9a',
-     }
-   },
- },
- plugins: [],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        red: { 
+          100: '#fbe9e9',
+          200: '#f4bdbd',
+          300: '#ea7c7c',
+          500: '#e35151',
+          600: '#dc2626',
+          700: '#b01e1e',
+          800: '#841616',
+          900: '#580f0f'
+        },
+        red_light: '#e35151',
+        red_dark: '#b01e1e',
+        blue: {
+          100: '#e9e9fb',
+          200: '#bdbdf4',
+          500: '#5151e3',
+          600: '#2626dc',
+          700: '#1a1a9a',
+          800: '#1e1eb0'
+        },
+        blue_light: '#6767e6',
+        blue_dark: '#1a1a9a',
+      }
+    },
+  },
+  plugins: [],
 }
