7362e31ed5bf4366763af1d3dd5a64ccdb912585
Ivar <ivar.saris@blueelevation.nl>
2025-07-29 15:09:10 +0200

    create UsersOverviewComponent

---
+++ src/app/users/users-overview/users-overview.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { UsersService } from '../users.service';
+import { User } from '../user.model';
+
+@Component({
+  selector: 'app-users-overview',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './users-overview.component.html',
+  styleUrl: './users-overview.component.scss'
+})
+export class UsersOverviewComponent implements OnInit {
+  private usersService = inject(UsersService);
+
+  users: User[] = [];
+  currentUser: User | undefined;
+
+  ngOnInit() {
+    this.usersService.usersList$.subscribe(users => {
+      this.users = users;
+    });
+
+    this.usersService.currentUser$.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
+
+  get userQty(): number {
+    return this.users.length;
+  }
+
+  get activeUserCount(): number {
+    return this.users.filter(user => user.isActive).length;
+  }
+
+  get adminUserCount(): number {
+    return this.users.filter(user => user.role === 'admin').length;
+  }
+
+  formatDate(date: Date): string {
+    return new Date(date).toLocaleDateString();
+  }
+}
